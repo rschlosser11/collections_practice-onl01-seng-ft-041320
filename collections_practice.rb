d3269a7a2d3173b4c844ec12b7f5e769ba4a024c
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 16:47:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,6 +8,12 @@ def sort_array_desc(ints)
   end
 end
 
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array;

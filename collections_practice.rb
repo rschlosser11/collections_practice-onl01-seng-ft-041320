6bf82ba60a62916b066c10460e529e75626198ce
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 16:26:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(ints)
+  ints.sort;
+end
 
+def sort_array_desc(ints)
+  ints.sort do |a, b|
+    b <=> a
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[inex]
+end
